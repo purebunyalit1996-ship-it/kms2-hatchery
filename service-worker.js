3ae74019eb94182f60cc6b07154a265443f7d9a3
purebunyalit1996-ship-it <purebunyalit1996@gmail.com>
2026-06-03 18:15:31 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = "kms2-duty-v1";
-
+const CACHE_NAME = "kms2-duty-v2";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./employee.html",
+  "./login.html",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
