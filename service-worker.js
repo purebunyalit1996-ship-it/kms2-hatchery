ac33837a7c3813d7b3b7d78c510163ae1a025882
purebunyalit1996-ship-it <purebunyalit1996@gmail.com>
2026-06-05 16:43:45 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,29 @@
-const CACHE_NAME = "kms2-duty-v28";
-const FILES_TO_CACHE = ["./","./index.html?v=27","./employee.html?v=28","./login.html","./manifest.json"];
+const CACHE_NAME = "kms2-admin-v29";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./login.html",
+  "./index.html?v=27",
+  "./manifest.json"
+];
+
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
+
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
 });
+
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
