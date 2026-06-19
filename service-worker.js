36b96d0c9e9848b2d517792d2ae3238f4a0bc9e6
purebunyalit1996-ship-it <purebunyalit1996@gmail.com>
2026-06-19 21:20:41 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kms2-employee-multiday-datefix-v48";
+const CACHE_NAME = "kms2-employee-multiday-datefix-v49";
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
