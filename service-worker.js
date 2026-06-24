2cfbed361acbddf87ab17225813263f8ac80939e
purebunyalit1996-ship-it <purebunyalit1996@gmail.com>
2026-06-24 21:43:43 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kms2-ui-promax-cssonly-v68";
+const CACHE_NAME = "kms2-substitute-displayfix-v70";
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
