771d457ed73a4dc7bacb46099e1d5a7e5a269a3e
purebunyalit1996-ship-it <purebunyalit1996@gmail.com>
2026-06-26 19:38:44 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kms2-substitute-visual-highlight-v82";
+const CACHE_NAME = "kms2-hard-remove-old-substitute-form-v84";
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
